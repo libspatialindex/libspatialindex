8411daab73abf439b806f8a44e16a1009828cc32
Howard Butler <hobu.inc@gmail.com>
2011-10-06 13:41:09 -0500

    a fix for #1 because gcc 4.5.2 appears to have these defined in stdlib.h

--- include/tools/rand48.h
+++ include/tools/rand48.h
@@ -1,11 +1,37 @@
 #pragma once
 
+#ifndef srand48
 extern void srand48(long seed);
+#endif
+
+#ifndef seed48
 extern unsigned short *seed48(unsigned short xseed[3]);
+#endif
+
+#ifndef nrand48
 extern long nrand48(unsigned short xseed[3]);
+#endif
+
+#ifndef mrand48
 extern long mrand48(void);
+#endif
+
+#ifndef lrand48
 extern long lrand48(void);
+#endif
+
+#ifndef lcong48
 extern void lcong48(unsigned short p[7]);
+#endif
+
+#ifndef jrand48
 extern long jrand48(unsigned short xseed[3]);
+#endif
+
+#ifndef erand48
 extern double erand48(unsigned short xseed[3]);
+#endif
+
+#ifdef drand48
 extern double drand48(void);
+#endif
