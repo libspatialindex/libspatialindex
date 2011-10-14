807016b69781a3d62cd3fab108d313d06570a62e
Howard Butler <hobu.inc@gmail.com>
2011-10-14 12:12:36 -0500

    only redefine c++0x types if we're <= msvc_ver 1500

--- include/spatialindex/capi/sidx_config.h
+++ include/spatialindex/capi/sidx_config.h
@@ -29,15 +29,19 @@
 #define SIDX_CONFIG_H_INCLUDED
 
 
+
 #ifdef _MSC_VER
-   typedef __int8 int8_t;
-   typedef __int16 int16_t;
-   typedef __int32 int32_t;
-   typedef __int64 int64_t;
-   typedef unsigned __int8 uint8_t;
-   typedef unsigned __int16 uint16_t;
-   typedef unsigned __int32 uint32_t;
-   typedef unsigned __int64 uint64_t;
+
+#if _MSC_VER <= 1500
+  typedef __int8 int8_t;
+  typedef __int16 int16_t;
+  typedef __int32 int32_t;
+  typedef __int64 int64_t;
+  typedef unsigned __int8 uint8_t;
+  typedef unsigned __int16 uint16_t;
+  typedef unsigned __int32 uint32_t;
+  typedef unsigned __int64 uint64_t;
+#endif
 
    #include <windows.h>
    #define STRDUP _strdup
