b00fb771a0afcefe915c2280cc238a0b5a95bb62
Howard Butler <hobu.inc@gmail.com>
2012-11-04 12:43:28 -0600

    adapt a patch for #15 to be able to incorporate a more efficient containsWhatQuery method

--- src/rtree/RTree.cc
+++ src/rtree/RTree.cc
@@ -447,12 +447,63 @@ bool SpatialIndex::RTree::RTree::deleteData(const IShape& shape, id_type id)
 	return ret;
 }
 
+
 void SpatialIndex::RTree::RTree::containsWhatQuery(const IShape& query, IVisitor& v)
 {
 	if (query.getDimension() != m_dimension) throw Tools::IllegalArgumentException("containsWhatQuery: Shape has the wrong number of dimensions.");
-	rangeQuery(ContainmentQuery, query, v);
-}
 
+#ifdef HAVE_PTHREAD_H
+	Tools::SharedLock lock(&m_lock);
+#endif
+
+	try
+	{
+		std::stack<NodePtr> st;
+		NodePtr root = readNode(m_rootID);
+		st.push(root);
+
+		while (! st.empty())
+		{
+			NodePtr n = st.top(); st.pop();
+
+			if(n->m_level == 0)
+			{
+				v.visitNode(*n);
+
+				for (uint32_t cChild = 0; cChild < n->m_children; ++cChild)
+				{
+					if(query.containsShape(*(n->m_ptrMBR[cChild])))
+					{
+						Data data = Data(n->m_pDataLength[cChild], n->m_pData[cChild], *(n->m_ptrMBR[cChild]), n->m_pIdentifier[cChild]);
+						v.visitData(data);
+						++(m_stats.m_u64QueryResults);
+					}
+				}
+			}
+			else //not a leaf
+			{
+				if(query.containsShape(n->m_nodeMBR))
+				{
+					visitSubTree(n, v);
+				}
+				else if(query.intersectsShape(n->m_nodeMBR))
+				{
+					v.visitNode(*n);
+
+					for (uint32_t cChild = 0; cChild < n->m_children; ++cChild)
+					{
+						st.push(readNode(n->m_pIdentifier[cChild]));
+					}
+				}
+			}
+		}
+
+	}
+	catch (...)
+	{
+		throw;
+	}
+}
 void SpatialIndex::RTree::RTree::intersectsWithQuery(const IShape& query, IVisitor& v)
 {
 	if (query.getDimension() != m_dimension) throw Tools::IllegalArgumentException("intersectsWithQuery: Shape has the wrong number of dimensions.");
@@ -1419,6 +1470,35 @@ void SpatialIndex::RTree::RTree::selfJoinQuery(id_type id1, id_type id2, const R
 						selfJoinQuery(n1->m_pIdentifier[cChild1], n2->m_pIdentifier[cChild2], rr, vis);
 					}
 				}
+			}
+		}
+	}
+}
+
+void SpatialIndex::RTree::RTree::visitSubTree(NodePtr subTree, IVisitor& v)
+{
+	std::stack<NodePtr> st;
+	st.push(subTree);
+
+	while (! st.empty())
+	{
+		NodePtr n = st.top(); st.pop();
+		v.visitNode(*n);
+
+		if(n->m_level == 0)
+		{
+			for (uint32_t cChild = 0; cChild < n->m_children; ++cChild)
+			{
+				Data data = Data(n->m_pDataLength[cChild], n->m_pData[cChild], *(n->m_ptrMBR[cChild]), n->m_pIdentifier[cChild]);
+				v.visitData(data);
+				++(m_stats.m_u64QueryResults);
+			}
+		}
+		else
+		{
+			for (uint32_t cChild = 0; cChild < n->m_children; ++cChild)
+			{
+				st.push(readNode(n->m_pIdentifier[cChild]));
 			}
 		}
 	}
--- src/rtree/RTree.h
+++ src/rtree/RTree.h
@@ -97,7 +97,8 @@ namespace SpatialIndex
 
 			void rangeQuery(RangeQueryType type, const IShape& query, IVisitor& v);
 			void selfJoinQuery(id_type id1, id_type id2, const Region& r, IVisitor& vis);
-
+            void visitSubTree(NodePtr subTree, IVisitor& v);
+            
 			IStorageManager* m_pStorageManager;
 
 			id_type m_rootID, m_headerID;
