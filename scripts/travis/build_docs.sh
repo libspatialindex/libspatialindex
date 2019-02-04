6df53c646ac6b20ee5eb8037d37af0a66756704f
Howard Butler <howard@hobu.co>
2019-02-04 12:39:24 -0600

    another doxygen cycle

--- scripts/travis/build_docs.sh
+++ scripts/travis/build_docs.sh
@@ -3,6 +3,11 @@
 TRAVIS_BUILD_DIR=$1
 echo "building docs for $TRAVIS_BUILD_DIR/docs"
 docker run -v $TRAVIS_BUILD_DIR:/data -w /data/docs osgeo/proj-docs make doxygen
+
+mkdir -p $TRAVIS_BUILD_DIR/docs/build/doxygen
+cp -r $TRAVIS_BUILD_DIR/docs/doxygen/html $TRAVIS_BUILD_DIR/docs/build/doxygen
+ls $TRAVIS_BUILD_DIR/docs/build/doxygen
+
 docker run -v $TRAVIS_BUILD_DIR:/data -w /data/docs osgeo/proj-docs make html
 docker run -v $TRAVIS_BUILD_DIR:/data -w /data/docs osgeo/proj-docs make latexpdf
 
