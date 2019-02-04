145213ca34067d9820ec75d0de05e73007fbe3f3
Howard Butler <howard@hobu.co>
2019-02-04 13:01:54 -0600

    don't ls

--- scripts/travis/build_docs.sh
+++ scripts/travis/build_docs.sh
@@ -6,7 +6,6 @@ docker run -v $TRAVIS_BUILD_DIR:/data -w /data/docs osgeo/proj-docs make doxygen
 
 mkdir -p $TRAVIS_BUILD_DIR/docs/build/doxygen
 cp -r $TRAVIS_BUILD_DIR/docs/doxygen/html $TRAVIS_BUILD_DIR/docs/build/doxygen
-ls $TRAVIS_BUILD_DIR/docs/build/doxygen
 
 docker run -v $TRAVIS_BUILD_DIR:/data -w /data/docs osgeo/proj-docs make html
 docker run -v $TRAVIS_BUILD_DIR:/data -w /data/docs osgeo/proj-docs make latexpdf
