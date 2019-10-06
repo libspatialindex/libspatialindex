72692d012ffd7db10bfba05066a9689fc146e3bb
Howard Butler <howard@hobu.co>
2019-10-05 22:02:33 -0500

    Upgrade gtest to 1.10.0 (#165)

--- test/gtest/gtest-1.7.0/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/hobu/dev/git/libspatialindex/test/gtest/gtest-1.7.0
-# Build directory: /Users/hobu/dev/git/libspatialindex/test/gtest/gtest-1.7.0
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
