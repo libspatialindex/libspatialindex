3eaf566db38f610f00398450d3a3bd0d7eb06527
Mike Taves <mwtoews@gmail.com>
2020-06-04 09:23:37 +1200

    GTest: Workaround Clang 5.0 axp issue with define _POSIX_PATH_MAX 256

--- test/gtest/gtest-1.10.0/src/gtest-filepath.cc
+++ test/gtest/gtest-1.10.0/src/gtest-filepath.cc
@@ -52,6 +52,10 @@
 #elif defined(_XOPEN_PATH_MAX)
 # define GTEST_PATH_MAX_ _XOPEN_PATH_MAX
 #else
+// Workaround Clang 5.0 axp issue
+#ifndef _POSIX_PATH_MAX
+# define _POSIX_PATH_MAX 256
+#endif // _POSIX_PATH_MAX
 # define GTEST_PATH_MAX_ _POSIX_PATH_MAX
 #endif  // GTEST_OS_WINDOWS
 
