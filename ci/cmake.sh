b7cde19e49899a19c6d4c1ee0e8d9bd818b23db0
Howard Butler <hobu.inc@gmail.com>
2024-05-03 13:06:15 -0500

    longpaths

--- ci/cmake.sh
+++ ci/cmake.sh
@@ -2,7 +2,7 @@
 
 
 cmake   -G "Ninja"  \
-      -DCMAKE_BUILD_TYPE=Debug \
+      -DCMAKE_BUILD_TYPE=RelWithDebInfo \
       -DBUILD_SHARED_LIBS=$BUILD_SHARED_LIBS \
       -DBUILD_TESTING=ON \
       -DCMAKE_INSTALL_PREFIX=${CONDA_PREFIX} \
