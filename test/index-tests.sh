6eb4d9f9808dd0b70ed0fb0aa55dee5138758edb
Howard Butler <hobu.inc@gmail.com>
2024-05-05 19:31:41 -0500

    more testing

---
+++ test/index-tests.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+pushd geometry/test1
+./run
+popd
+pushd rtree/test1
+./run
+popd
+pushd rtree/test2
+./run
+popd
+pushd rtree/test3
+./run
+popd
+pushd rtree/test4
+./run
+popd
+pushd tprtree/test1
+./run
+popd
+pushd tprtree/test2
+./run
+popd
+pushd mvrtree/test1
+./run
+popd
+pushd mvrtree/test2
+./run
+popd
